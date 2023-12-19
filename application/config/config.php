d1264370da85ee9bf3c73c0080bc74d0850d5b23
Yoga Pratama Royaldy <75155420+Royaldyx@users.noreply.github.com>
2023-12-19 16:29:18 +0700

    Update config.php
    update url terbaru

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/tokojayaa/';
-// ini update terbaru untuk angga
+// ini update terbaru untuk angga dan javie
 
 /*
 |--------------------------------------------------------------------------
